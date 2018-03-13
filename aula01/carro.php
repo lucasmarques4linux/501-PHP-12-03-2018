c6a29a419af0078ae7a2c0e45d629965c3384249
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-03-12 21:59:39 -0300

    aula01

---
+++ aula01/carro.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Classe Carro
+// Atributos privados cor, marca, velocidade, marcha;
+// Construtor que define uma cor e uma marca para o carro;
+// Métodos publicos,verCor,verMarca,verVelocidade,verMarcha;
+// Método publico acelelar - aumenta a velocidade em 10
+// Método publico freiar - diminui a velocidade em 10
+// Método publico trocarMarcha - recebe uma marcha por parametro e altera;
+
---
+++ aula01/primeiro-objeto.php
@@ -0,0 +1,38 @@
+<?php 
+echo "<pre>";
+
+class Carro
+{
+	public $cor;
+	public $marca;
+	public $modelo;
+	public $velocidade;
+
+	public function buzinar(){
+		echo "Estou buzinando <br>";
+	}
+	public function acelerar(){
+		$this->velocidade += 10;
+	}
+	public function freiar(){
+		$this->velocidade -= 10;
+	}
+}
+
+$uno = new Carro();
+$uno->cor = "Azul";
+$uno->marca = "Fiat";
+$uno->modelo = "Uno 1.0";
+$uno->buzinar();
+$uno->acelerar();
+$uno->acelerar();
+$uno->acelerar();
+echo "Velocidade Atual: " . $uno->velocidade . "<br>";
+$uno->freiar();
+echo "Velocidade Atual: " . $uno->velocidade . "<br>";
+var_dump($uno);
+// $gol = new Carro();
+// $gol->cor = "Preto";
+// $gol->marca = "VW";
+// $gol->modelo = "Gol 1.4";
+// var_dump($gol);
