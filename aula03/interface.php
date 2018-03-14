8619e299bed239a1eeecf8f135e29969c72cd2c1
Lucas Marques <lucasmarques73@hotmail.com>
2018-03-14 20:43:44 -0300

    aula03

---
+++ aula03/interface.php
@@ -0,0 +1,15 @@
+<?php 
+
+interface ContaInterface
+{
+	public function sacar();
+	public function depositar();
+	public function verSaldo();
+}
+
+class Conta implements ContaInterface
+{
+	public function sacar(){}
+	public function depositar(){}
+	public function verSaldo(){}
+}
---
+++ aula03/tv.php
@@ -0,0 +1,57 @@
+<?php 
+
+interface TelevisaoInterface
+{
+	public function aumentarVolume();
+	public function diminuirVolume();
+	public function ligar();
+	public function desligar();
+	public function trocarCanal($canal);
+}
+interface txt
+{
+	public function test();
+}
+
+class TV implements TelevisaoInterface,txt
+{
+	public $volume = 10;
+	public $canal = 5;
+	public $estado = false;
+
+	public function aumentarVolume(){
+		$this->volume++;
+	}
+	public function diminuirVolume(){
+		$this->volume--;
+	}
+	public function ligar(){
+		$this->estado = true;
+	}
+	public function desligar(){
+		$this->estado = false;
+	}
+	public function trocarCanal($canal){
+		$this->canal = $canal;
+	}
+}
+echo "<pre>";
+$tv = new TV();
+print_r($tv);
+echo "<hr>";
+$tv->ligar();
+print_r($tv);
+echo "<hr>";
+$tv->aumentarVolume();
+$tv->aumentarVolume();
+$tv->aumentarVolume();
+print_r($tv);
+echo "<hr>";
+$tv->diminuirVolume();
+print_r($tv);
+echo "<hr>";
+$tv->trocarCanal(11);
+print_r($tv);
+echo "<hr>";
+$tv->desligar();
+print_r($tv);
