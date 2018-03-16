064797e9c7b20dfb471db479cf18e32b27719340
Lucas Marques <lucasmarques73@hotmail.com>
2018-03-15 21:07:55 -0300

    exception

---
+++ aula04/exception/ex.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Soma
+// Não permitir número negativos
+// Não pertitir resultado maior que 50
+// Subtração
+// Não permitir resultado menor que zero
+// Divisão
+// Não permitir divisão por zero
+// Não permitir valor Infinito $a == 0
+// Multiplicação
+// Não permitir multiplicação por zero
+// Não permitir resultado maior que 50
--- aula04/static/calc.php
+++ aula04/static/calc.php
@@ -5,6 +5,22 @@ class Calc
 	public static function soma(int $a,int $b){
 		return $a + $b;
 	}
+	public static function subt(int $a,int $b){
+		return $a - $b;
+	}
+	public static function mult(int $a,int $b){
+		return $a * $b;
+	}
+	public static function divi(int $a,int $b){
+		return $a / $b;
+	}
 }
 
 echo Calc::soma(1,2);
+echo "<br>";
+echo Calc::subt(1,2);
+echo "<br>";
+echo Calc::mult(1,2);
+echo "<br>";
+echo Calc::divi(1,2);
+echo "<br>";
