a2868d2bab0dbe2e163918bed0496b01ab70069d
Lucas Marques <lucasmarques73@hotmail.com>
2018-03-22 20:03:51 -0300

    ad

---
+++ aula09/mvc/public/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+require '../config/config.php';
+require '../autoload.php';
+
+use Lib\FrontController\FrontController;
+
+// Rotas
+// usuarios 	 -> lista de Usuarios
+// usuarios/new  -> Criar Usuario
+// usuarios/edit -> Editar Usuario
+$rota = 'usuarios/edit';
+
+(new FrontController())->run($rota);
