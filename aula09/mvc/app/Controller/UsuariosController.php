dab37c74e5996fddff93242ebd9711eb05f0bbb0
Lucas Marques <lucasmarques73@hotmail.com>
2018-03-22 21:38:10 -0300

    finish front controller

--- aula09/mvc/app/Controller/UsuariosController.php
+++ aula09/mvc/app/Controller/UsuariosController.php
@@ -2,13 +2,30 @@
 
 namespace Controller;
 
+use Model\UsuariosModel;
+use Lib\BaseView\BaseView;
+
 class UsuariosController
 {
+	private $usuariosModel;
+	private $baseView;
+
+	public function __construct(){
+		$this->usuariosModel = new UsuariosModel();
+		$this->baseView = new BaseView();
+	}
+
 	public function index(){
-		echo "Index";
+		$usuarios = $this->usuariosModel->all();
+		$this->baseView->render('usuarios/index',
+			['usuarios' => $usuarios]);
 	}
 
 	public function new(){
-		echo "NEW";
+		echo "Controller - New";
+	}
+
+	public function edit($id){
+		echo "Controller - Edit {$id}";
 	}
 }
--- aula09/mvc/public/index.php
+++ aula09/mvc/public/index.php
@@ -5,10 +5,4 @@ require '../autoload.php';
 
 use Lib\FrontController\FrontController;
 
-// Rotas
-// usuarios 	 -> lista de Usuarios
-// usuarios/new  -> Criar Usuario
-// usuarios/edit -> Editar Usuario
-
-
 (new FrontController())->run();
